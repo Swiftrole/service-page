4474b0f060c326c70d7a13ba4692ce4cec33cb21
ismail <ismailogunshina@gmail.com>
2023-08-29 21:58:49 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          DEFAULT: '#413DEE',
+          '500': '#413DEE'
+        }
+    },
+  },
+  plugins: [],
+}
+}
